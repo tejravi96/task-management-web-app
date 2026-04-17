c3c0e97e17782ad1a515b45d773ff78dc9416d14
ANNU NARAYANA RAVITEJA <99885368+tejravi96@users.noreply.github.com>
2026-04-17 15:49:44 +0530

    Refactor TaskComponent methods for consistency

--- task-management-web-app/frontend/src/app/task.component.ts
+++ task-management-web-app/frontend/src/app/task.component.ts
@@ -1,33 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskService } from './task.service';
+
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html'
 })
 export class TaskComponent implements OnInit {
+
   tasks: any[] = [];
   newTask: any = { id: null, title: '', description: '' };
+
   constructor(private service: TaskService) {}
-  ngOnInit() { this.loadTasks(); }
-  loadTasks() {
-    this.service.getTasks().subscribe((data: any) => { this.tasks = data; });
+
+  ngOnInit() {
+    this.loadTasks();
   }
+
+  loadTasks() {
+    this.service.getTasks().subscribe((data: any) => {
+      this.tasks = data;
+    });
+  }
+
   addTask() {
     this.service.addTask(this.newTask).subscribe(() => {
       this.loadTasks();
-      this.newTask = { title: '', description: '' };
+      this.newTask = { id: null, title: '', description: '' };
     });
   }
+
   deleteTask(id: number) {
-    this.service.deleteTask(id).subscribe(() => { this.loadTasks(); });
+    this.service.deleteTask(id).subscribe(() => {
+      this.loadTasks();
+    });
   }
+
   editTask(task: any) {
-  this.newTask = { ...task };
-}
+    this.newTask = { ...task };
+  }
+
   updateTask() {
-  this.service.updateTask(this.newTask.id, this.newTask).subscribe(() => {
-    this.loadTasks();
-    this.newTask = { id: null, title: '', description: '' };
-  });
-}
+    this.service.updateTask(this.newTask.id, this.newTask).subscribe(() => {
+      this.loadTasks();
+      this.newTask = { id: null, title: '', description: '' };
+    });
+  }
 }
